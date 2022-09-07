011dfa7032c5811c4d8509d27c4681015fa446f7
obezglavlen <pcgalaxy123@gmail.com>
2022-09-07 21:04:36 +0300

    add sorting

--- routes/animeList/getAll.js
+++ routes/animeList/getAll.js
@@ -1,10 +1,47 @@
 const { Anime } = require("../../models");
 
+const SORT = {
+  NAME: "NAME",
+  RATING: "RATING",
+};
+const SORT_ORDER = {
+  ASC: "ASC",
+  DESC: "DESC",
+};
+
+const sortCondition = (sort = SORT.NAME, order = SORT_ORDER.ASC) => {
+  switch (sort) {
+    case SORT.NAME:
+      return (a, b) => {
+        switch (order) {
+          case SORT_ORDER.ASC:
+            return -1 * a.title.localeCompare(b);
+          case SORT_ORDER.DESC:
+            return a.title.localeCompare(b);
+          default:
+            return -1 * a.title.localeCompare(b);
+        }
+      };
+    case SORT.RATING:
+      return (a, b) => {
+        switch (order) {
+          case SORT_ORDER.ASC:
+            return a.rating < b.rating ? -1 : 1;
+          case SORT_ORDER.DESC:
+            return a.rating < b.rating ? 1 : -1;
+          default:
+            return a.rating < b.rating ? 1 : -1;
+        }
+      };
+  }
+};
+
 module.exports = {
   method: "get",
   route: "/animeLists/:id",
   handler: async (req, res, next) => {
     const userId = req.params.id;
+    const { sort, sortOrder } = req.query;
 
     const animes = await Anime.findById(userId);
 
@@ -28,7 +65,7 @@ module.exports = {
     for (const key in animes.toJSON()) {
       const sortedArray = animes.toJSON()[key];
       try {
-        sortedArray.sort();
+        sortedArray.sort(sortCondition(sort, sortOrder));
       } catch {
         continue;
       }
